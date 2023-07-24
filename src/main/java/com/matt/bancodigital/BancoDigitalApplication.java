84bd2bb8d37632e4c107e059cb53772c1bbd3366
MatheusBrito21 <matheusb790@gmail.com>
2023-07-24 18:54:42 -0300

    delete draw

--- src/main/java/com/matt/bancodigital/BancoDigitalApplication.java
+++ src/main/java/com/matt/bancodigital/BancoDigitalApplication.java
@@ -2,8 +2,9 @@ package com.matt.bancodigital;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class BancoDigitalApplication {
 
 	public static void main(String[] args) {
